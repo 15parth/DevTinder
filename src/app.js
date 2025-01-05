f73ceec6a5090425bd49823244c12aa376506800
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-01-05 23:06:32 +0530

    intial requests and postman api testing

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
-const express=  require('express')
+const express = require('express');
+const app = express();
 
 
-const app = express()
-
-
-
-app.use((req, res)=>{
-    res.send('Hello World')
+app.use('/hello',(req,res)=>{
+    res.send('this is a default  code motherfuckers');
 })
 
-app.listen(3000, ()=>{
-    console.log('my server is runnig on port 3000')
+app.post('/hello', (req, res) => {
+    const response = {
+        firstName: 'Parth',
+        lastName: 'Bhardwaj'
+    };
+    res.send(`${JSON.stringify(response)} this is a POST request!`);
+});
+
+app.delete('/hello',(req, res)=>{
+    res.send('This is a DELETE request!');
 })
 
-// console.log(app)
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
